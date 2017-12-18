2278acf2ad92c06c468cb6966c7a9b382dd0f722
RJ.Hwang <rongjihuang@gmail.com>
2017-12-18 10:35:48 +0800

    Add String to SHA256 test method

--- src/test/java/tech/simter/jwt/signer/HS256Test.java
+++ src/test/java/tech/simter/jwt/signer/HS256Test.java
@@ -5,7 +5,10 @@ import tech.simter.jwt.Algorithm;
 import tech.simter.jwt.Signer;
 import tech.simter.jwt.SignerFactory;
 
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,5 +25,32 @@ public class HS256Test {
     byte[] key = "test".getBytes(StandardCharsets.UTF_8);
     byte[] sign = signer.sign(data, key);
     assertEquals("8Yr6vjx78DFGAaXJ_j6Wa5kq82wa1AFyZzyMXtOs91Y=", new String(sign, StandardCharsets.UTF_8));
+  }
+
+  @Test
+  public void string2sha256() throws NoSuchAlgorithmException {
+    String text = "simter-demo";
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    Charset utf8 = StandardCharsets.UTF_8;
+    byte[] hash = digest.digest(text.getBytes(utf8));
+
+    // byte to hex
+    System.out.println(bytesToHex(hash).toUpperCase());
+  }
+
+  private static String bytesToHex(byte[] bytes) {
+    StringBuilder result = new StringBuilder();
+    for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+    return result.toString();
+  }
+
+  private static String bytesToHex2(byte[] bytes) {
+    StringBuilder hexString = new StringBuilder();
+    for (byte b : bytes) {
+      String hex = Integer.toHexString(0xff & b);
+      if (hex.length() == 1) hexString.append('0');
+      hexString.append(hex);
+    }
+    return hexString.toString();
   }
 }
